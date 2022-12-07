231e60232fe8ef9e0df2a773e3b911c2d0afe381
JGrater <119420246+JGrater@users.noreply.github.com>
2022-12-07 16:13:55 +0000

    Hit and Destroy detection

--- src/main/java/ButtonPressed.java
+++ src/main/java/ButtonPressed.java
@@ -18,10 +18,11 @@ public class ButtonPressed implements ActionListener{
             this.grid = grid;
             
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
         adaShipConfig = AdaShipConfig.getInstance();
+        int[] coords = {row, col};
         switch(grid[row][col]) {
             case AdaShipConfig.OCEAN:
                 button.setBackground(Color.white);
@@ -36,9 +37,16 @@ public class ButtonPressed implements ActionListener{
                 grid[row][col] = AdaShipConfig.HIT;
                 adaShipConfig.setEnemyGrid(grid);
                 // Need to know which ship is hit, then lower its health by 1
+                for (int i = 0; i < adaShipConfig.getEnemyFleet().size(); i++) {
+                    if (adaShipConfig.getEnemyFleet().get(i).checkHit(coords)) {
+                        System.out.println("HIT!!");
+                        if (adaShipConfig.getEnemyFleet().get(i).checkDestroyed()) {
+                            System.out.println("SHIP SUNK!!!");
+                        }
+                    }
+                }
                 System.out.println("Col: " + col + "Row: "+ row);
                 break;
         }
     }
-    
 }
