8500f9df7a704fd75b9d4b9b398d7573bb761de1
JGrater <119420246+JGrater@users.noreply.github.com>
2022-12-19 18:15:30 +0000

    Win Screen and refactor

--- src/main/java/NewGame.java
+++ src/main/java/NewGame.java
@@ -3,6 +3,8 @@ package main.java;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -10,21 +12,28 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 
-public class NewGame extends JPanel {
+public class NewGame extends JPanel implements ActionListener{
     AdaShipConfig adaShipConfig = AdaShipConfig.getInstance();
+    JPanel header, footer;
+    JLabel instruction;
+    JButton randomPlay, play, exit;
+    NewShipPanel[] fleet;
+
     public NewGame() {
+        header = new JPanel();
+        footer = new JPanel();
+        instruction = new JLabel();
+        randomPlay = new JButton();
+        play = new JButton();
+        exit = new JButton();
+
         setPreferredSize(AdaShipConfig.getDimension(750,500));
         setLayout(AdaShipConfig.getFlowLayout(FlowLayout.CENTER, 0, 30));
         setBackground(Color.red);
         setVisible(false);
-        build();
     }
 
     public void build() {
-        JPanel header = new JPanel();
-        JPanel footer = new JPanel();
-        JLabel instruction = new JLabel();
-
         instruction.setText("Pick a starting coordinate and a direction to face your ship!");
         instruction.setFont(AdaShipConfig.getFont("Calibri", Font.BOLD, 25));
         instruction.setForeground(Color.WHITE);
@@ -35,19 +44,15 @@ public class NewGame extends JPanel {
         header.add(instruction);
         add(header);
 
-        MenuShip[] fleet = new MenuShip[adaShipConfig.getFleet().size()];
+        fleet = new NewShipPanel[adaShipConfig.getFleet().size()];
         for (int i = 0; i < fleet.length; i++) {
-            fleet[i] = new MenuShip(adaShipConfig.getFleet().get(i));
+            fleet[i] = new NewShipPanel(adaShipConfig.getFleet().get(i));
             add(fleet[i]);
         }
 
         footer.setPreferredSize(AdaShipConfig.getDimension(750, 40));
         footer.setBackground(Color.red);
         footer.setLayout(AdaShipConfig.getFlowLayout(FlowLayout.RIGHT, 50, 2));
-
-        JButton randomPlay = new JButton();
-        JButton play = new JButton();
-        JButton exit = new JButton();
 
         exit.setText("Exit");
         exit.setFont(AdaShipConfig.getFont("Calibri", Font.BOLD, 18));
@@ -69,31 +74,11 @@ public class NewGame extends JPanel {
         footer.add(exit);
         add(footer);
     }
-}
 
-class MenuShip extends JPanel {
-
-    private Ship ship;
-
-    public MenuShip(Ship ship) {
-        this.ship = ship;
-        setPreferredSize(AdaShipConfig.getDimension(750, 40));
-        setLayout(AdaShipConfig.getFlowLayout(1, 100, 5));
-        setBackground(Color.red);
-        String directions[] = {"NORTH", "SOUTH", "EAST", "WEST"};
-        JTextField coordinateField = new JTextField();
-        JComboBox<String> directionBox = new JComboBox<>(directions);
-        JLabel shipLabel = new JLabel();
-
-        shipLabel.setPreferredSize(AdaShipConfig.getDimension(270, 25));
-        shipLabel.setText("Name: " + ship.getType() + "  ,  Length: " + ship.getHealth());
-        shipLabel.setFont(AdaShipConfig.getFont("Calibri", Font.BOLD, 20));
-        shipLabel.setForeground(Color.WHITE);
-        shipLabel.setHorizontalAlignment(JLabel.LEFT);
-
-        coordinateField.setPreferredSize(AdaShipConfig.getDimension(100, 25));
-        add(shipLabel);
-        add(coordinateField);
-        add(directionBox);
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == exit) {
+            adaShipConfig.getGame().runMenu();
+        }
     }
 }
